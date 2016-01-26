f78d5216c0f218188ccfdc465c595edb3790a2c5
Txus Ballesteros <txus.ballesteros@gmail.com>
2016-01-26 21:23:14 +0100

    Building Project Structure

--- app/src/main/java/com/txusballesteros/demo/MainActivity.java
+++ app/src/main/java/com/txusballesteros/demo/MainActivity.java
@@ -32,14 +32,12 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initialzieActivity();
+        initializeActivity();
         setContentView(R.layout.activity_main);
     }
 
-    private void initialzieActivity() {
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
+    private void initializeActivity() {
+        getWindow().getDecorView()
+                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 }
